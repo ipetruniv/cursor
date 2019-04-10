69f7964c9379d81fe1ab460cb5a9d949b8d73ebb
Ihor Petruniv <ipetruniv@gmail.com>
2019-04-10 21:21:12 +0300

    Make code better rev01

--- homework_01/temp.py
+++
@@ -1,7 +0,0 @@
-myalpha = {}
-for i, value in enumerate("abcdefghijklmnopqrstuvwxyz", 1):
-    #print(i, value)
-    myalpha[value] = i
-print(myalpha)
-
-print(58221.82 + 32.73 + 260.45 + 61939 + 53800)*0.05)
